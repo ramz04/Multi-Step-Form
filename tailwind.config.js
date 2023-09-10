6c827f5e5d15a7f76174d3c8b2e26eaab6ba907d
ramz04 <wunpini95@gmail.com>
2023-09-10 13:04:19 -0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        Marineblue: "hsl(213, 96%, 18%)",
+        Purplishblue: "hsl(243, 100%, 62%)",
+        Pastelblue: "hsl(228, 100%, 84%)",
+        Lightblue: "hsl(206, 94%, 87%)",
+        Strawberryred: "hsl(354, 84%, 57%)",
+
+        Coolgray: "hsl(231, 11%, 63%)",
+        Lightgray: "hsl(229, 24%, 87%)",
+        Magnolia: "hsl(217, 100%, 97%)",
+        Alabaster: "hsl(231, 100%, 99%)",
+        white: "hsl(0, 0%, 100%)"
+      }
     },
   },
   plugins: [],
