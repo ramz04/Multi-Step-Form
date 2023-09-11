2cba75c2c53bba0514d07edbedb845c291ca4f5c
ramz04 <wunpini95@gmail.com>
2023-09-10 20:37:43 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,29 @@
+"use client";
+
 import Image from "next/image";
 import bg_sidebar_mobile from "./assets/images/bg-sidebar-mobile.svg";
 import bg_sidebar_desktop from "./assets/images/bg-sidebar-desktop.svg";
 import { Button } from "@/components/ui/button";
 import PersonalInfo from "@/components/forms/PersonalInfo";
 import { FormItems, planOptions } from "../../types";
+import { useState } from "react";
+
+const initialValues: FormItems = {
+  name: "",
+  email: "",
+  phone: "",
+  plan: "Arcade",
+  planLength: false,
+  isLargerStorage: false,
+  isCustomizableProfile: false,
+  isOnlineService: false,
+};
+
+const sideBar = ["Your Info", "Select Plan", "Add-Ons", "Summary"];
 
 export default function Home() {
-  const initialValues: FormItems = {
-    name: "",
-    email: "",
-    phone: "",
-    plan: "Arcade",
-    planLength: false,
-    isLargerStorage: false,
-    isCustomizableProfile: false,
-    isOnlineService: false,
-  };
+  const [formData, setFormData] = useState(initialValues);
+
   return (
     <main className=" relative flex h-screen w-full flex-col bg-Magnolia">
       <div className="">
