cd5108b3654938d50aeed805a7a737f1342de0c2
ramz04 <wunpini95@gmail.com>
2023-09-10 20:34:35 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,21 @@
-"use client";
-
 import Image from "next/image";
 import bg_sidebar_mobile from "./assets/images/bg-sidebar-mobile.svg";
 import bg_sidebar_desktop from "./assets/images/bg-sidebar-desktop.svg";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useForm } from "react-hook-form";
-import type { FieldValues } from "react-hook-form";
-import { resolve } from "path";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-
-const personalInfoSchema = z.object({
-  name: z.string().length(100),
-  email: z.string().email(),
-  phone: z.number().gt(5, "Phone Number must be 10 numbers"),
-});
+import PersonalInfo from "@/components/forms/PersonalInfo";
+import { FormItems, planOptions } from "../../types";
 
 export default function Home() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-    reset,
-    getValues,
-  } = useForm({
-    resolver: zodResolver(personalInfoSchema),
-  });
-
-  const onSubmit = async (data: FieldValues) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    reset();
+  const initialValues: FormItems = {
+    name: "",
+    email: "",
+    phone: "",
+    plan: "Arcade",
+    planLength: false,
+    isLargerStorage: false,
+    isCustomizableProfile: false,
+    isOnlineService: false,
   };
-
   return (
     <main className=" relative flex h-screen w-full flex-col bg-Magnolia">
       <div className="">
@@ -45,78 +27,11 @@ export default function Home() {
         <div></div>
       </div>
       <div className="-mt-24 flex justify-center px-3 py-6">
-        <div className="w-full space-y-2 rounded-lg bg-white px-4 py-6 shadow-sm">
-          <h1 className="text-xl font-bold tracking-wide text-Marineblue">
-            Personal Info
-          </h1>
-          <p className="w-[80%] tracking-wide text-Coolgray">
-            Please provide your name, email address, and phone number.
-          </p>
-          <div>
-            <div className="space-y-3">
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="name" className="text-sm text-Marineblue">
-                    Name
-                  </label>
-                  {errors.name && (
-                    <p className="text-sm text-red-500"> Name is required</p>
-                  )}
-                </div>
-                <Input
-                  {...register("name")}
-                  id="name"
-                  name="name"
-                  type="text"
-                  placeholder="e.g. Stephen King"
-                />
-              </div>
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="email" className="text-sm text-Marineblue">
-                    Email Address
-                  </label>
-                  {errors.email && (
-                    <p className="text-sm text-red-500"> Email is required</p>
-                  )}
-                </div>
-                <Input
-                  {...register("email")}
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="e.g. stephenking@lorem.com"
-                />
-              </div>
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="phone" className="text-sm text-Marineblue">
-                    Phone Number
-                  </label>
-                  {errors.phone && (
-                    <p className="text-sm text-red-500">
-                      Phone Number is required
-                    </p>
-                  )}
-                </div>
-                <Input
-                  {...register("phone")}
-                  id="phone"
-                  name="phone"
-                  type="number"
-                  placeholder="e.g. +1 234 567 890"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+        <PersonalInfo />
       </div>
+
       <div className="mt-auto flex items-end justify-end p-4">
-        <Button
-          type="submit"
-          disabled={isSubmitting}
-          className="bg-Marineblue text-Magnolia"
-        >
+        <Button type="submit" className="bg-Marineblue text-Magnolia">
           Confirm
         </Button>
       </div>
---
+++ src/components/forms/PersonalInfo.tsx
@@ -0,0 +1,105 @@
+"use client";
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import type { FieldValues } from "react-hook-form";
+import { resolve } from "path";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const personalInfoSchema = z.object({
+  name: z.string().length(100),
+  email: z.string().email(),
+  phone: z.number().gt(5, "Phone Number must be 10 numbers"),
+});
+
+type personalInfoSchema = z.infer<typeof personalInfoSchema>;
+
+export default function PersonalInfo() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    getValues,
+  } = useForm<personalInfoSchema>({
+    resolver: zodResolver(personalInfoSchema),
+  });
+
+  const onSubmit = async (data: personalInfoSchema) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    reset();
+  };
+
+  return (
+    <div>
+      <div className="w-full space-y-2 rounded-lg bg-white px-4 py-6 shadow-sm">
+        <h1 className="text-xl font-bold tracking-wide text-Marineblue">
+          Personal Info
+        </h1>
+        <p className="w-[80%] tracking-wide text-Coolgray">
+          Please provide your name, email address, and phone number.
+        </p>
+        <div>
+          <div className="space-y-3">
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="name" className="text-sm text-Marineblue">
+                  Name
+                </label>
+                {errors.name && (
+                  <p className="text-sm text-red-500"> Name is required</p>
+                )}
+              </div>
+              <Input
+                {...register("name")}
+                id="name"
+                name="name"
+                type="text"
+                placeholder="e.g. Stephen King"
+              />
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="email" className="text-sm text-Marineblue">
+                  Email Address
+                </label>
+                {errors.email && (
+                  <p className="text-sm text-red-500"> Email is required</p>
+                )}
+              </div>
+              <Input
+                {...register("email")}
+                id="email"
+                name="email"
+                type="email"
+                placeholder="e.g. stephenking@lorem.com"
+              />
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="phone" className="text-sm text-Marineblue">
+                  Phone Number
+                </label>
+                {errors.phone && (
+                  <p className="text-sm text-red-500">
+                    Phone Number is required
+                  </p>
+                )}
+              </div>
+              <Input
+                {...register("phone")}
+                id="phone"
+                name="phone"
+                type="number"
+                placeholder="e.g. +1 234 567 890"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
