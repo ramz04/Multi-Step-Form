367399af5875af9755f65c002bb8fccaa8976354
ramz04 <wunpini95@gmail.com>
2023-09-12 20:41:47 +0000

    update

--- src/components/forms/AddOns.tsx
+++ src/components/forms/AddOns.tsx
@@ -14,7 +14,19 @@ export default function AddOns({
 }: StepProps) {
   return (
     <div className="py-2">
-      <div className="w-full space-y-2 rounded-lg bg-white px-4 py-4 shadow-sm"></div>
+      <div className="w-full space-y-2 rounded-lg bg-white px-4 py-4 shadow-sm">
+        <h1 className="text-2xl font-bold tracking-wide text-Marineblue">
+          Pick add-ons
+        </h1>
+        <p className="w-[85%] tracking-wide text-Coolgray">
+          Add-ons help enhance your gaming experience.
+        </p>
+        <div className="space-y-3 pt-1">
+          <div
+            className={`flex gap-3 rounded-md border border-gray-300  px-3 py-3`}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
