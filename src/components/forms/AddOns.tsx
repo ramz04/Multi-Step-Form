6f300d747f6f3d4015092985f8c91620948029a0
ramz04 <wunpini95@gmail.com>
2023-09-11 23:06:06 -0700

    update

--- src/components/forms/AddOns.tsx
+++ src/components/forms/AddOns.tsx
@@ -12,5 +12,9 @@ export default function AddOns({
   updateForm,
   planLength,
 }: StepProps) {
-  return <div>AddOns</div>;
+  return (
+    <div className="py-2">
+      <div className="w-full space-y-2 rounded-lg bg-white px-4 py-4 shadow-sm"></div>
+    </div>
+  );
 }
--- src/components/forms/FinishingUp.tsx
+++ src/components/forms/FinishingUp.tsx
@@ -13,5 +13,9 @@ export default function FinishingUp({
   isLargerStorage,
   isOnlineService,
 }: StepProps) {
-  return <div>FinishingUp</div>;
+  return (
+    <div className="py-2">
+      <div className="w-full space-y-2 rounded-lg bg-white px-4 py-4 shadow-sm"></div>
+    </div>
+  );
 }
