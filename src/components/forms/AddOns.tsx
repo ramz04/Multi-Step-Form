0ec7352e47ddd861ce8fc155c1b0f1b2e9a68df7
ramz04 <wunpini95@gmail.com>
2023-09-14 19:52:24 +0000

    update

--- src/components/forms/AddOns.tsx
+++ src/components/forms/AddOns.tsx
@@ -15,7 +15,7 @@ export default function AddOns({
 }: StepProps) {
   return (
     <div className="py-2">
-      <div className="w-full space-y-2 rounded-lg bg-white px-4 py-4 shadow-sm">
+      <div className="w-full space-y-2 rounded-lg bg-white px-4 py-6 shadow-sm">
         <h1 className="text-2xl font-bold tracking-wide text-Marineblue">
           Pick add-ons
         </h1>
@@ -26,8 +26,57 @@ export default function AddOns({
           <div
             className={`flex gap-3 rounded-md border border-gray-300  px-3 py-3`}
           >
-            <div>
-              <Checkbox className="h-4 w-4" />
+            <div className="flex w-full items-center gap-3">
+              <div>
+                <Checkbox
+                  className="h-[18px] w-[18px] rounded"
+                  checked={isOnlineService}
+                  onCheckedChange={(newIsOnlineServices) =>
+                    updateForm({
+                      isOnlineService: newIsOnlineServices,
+                    })
+                  }
+                />
+              </div>
+              <div className="">
+                <h1 className="font-bold text-Marineblue">Online service</h1>
+                <p className="text-sm text-Coolgray">
+                  Access to multiplayer games
+                </p>
+              </div>
+              <p className="ml-auto text-sm text-Purplishblue">+$1/mo</p>
+            </div>
+          </div>
+          <div
+            className={`flex gap-3 rounded-md border border-gray-300  px-3 py-3`}
+          >
+            <div className="flex w-full items-center gap-3">
+              <div>
+                <Checkbox className="h-[18px] w-[18px] rounded" />
+              </div>
+              <div className="">
+                <h1 className="font-bold text-Marineblue">Larger storage</h1>
+                <p className="text-sm text-Coolgray">Extra 1TB of cloud save</p>
+              </div>
+              <p className="ml-auto text-sm text-Purplishblue">+$2/mo</p>
+            </div>
+          </div>
+          <div
+            className={`flex gap-3 rounded-md border border-gray-300  px-3 py-3`}
+          >
+            <div className="flex w-full items-center gap-3">
+              <div>
+                <Checkbox className="h-[18px] w-[18px] rounded" />
+              </div>
+              <div className="">
+                <h1 className="font-bold text-Marineblue">
+                  Customizable Profile
+                </h1>
+                <p className="text-sm text-Coolgray">
+                  Custom theme on your profile
+                </p>
+              </div>
+              <p className="ml-auto text-sm text-Purplishblue">+$2/mo</p>
             </div>
           </div>
         </div>
--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -13,7 +13,7 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "border-primary ring-offset-background focus-visible:ring-ring peer h-4 w-4 shrink-0 rounded-sm border focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-Marineblue data-[state=checked]:text-slate-50",
+      "peer h-4 w-4 shrink-0 rounded-sm border border-gray-300 shadow-sm focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-Purplishblue disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-Purplishblue data-[state=checked]:text-slate-50 dark:border-Purplishblue dark:border-slate-50 dark:focus-visible:ring-slate-300 dark:data-[state=checked]:bg-slate-50 dark:data-[state=checked]:text-Purplishblue",
       className,
     )}
     {...props}
